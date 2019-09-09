78c478e7396a10c0db5e006d8e00d68a1c75dc5d
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-09 08:32:49 -0700

    more boilerplate

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 const MainComponent = (props) => {
     return ( <div>MainComponent</div> );
 }
+
+const mapStateToProps = state =>{
+    return { 
+        state
+    }
+}
  
-export default MainComponent;
+export default connect(mapStateToProps, {})(MainComponent);
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
